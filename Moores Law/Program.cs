0cc2507a60fe6676b6c4a656d72eb221f9363db5
Lungisani M Nhlengethwa <lm.nhlengethwa@outlook.com>
2021-02-22 08:16:48 +0200

    write to csv

--- Moores Law/Program.cs
+++ Moores Law/Program.cs
@@ -2,7 +2,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using static Moores_Law.Utilities.Enums;
 
 namespace Moores_Law
@@ -12,19 +14,27 @@ namespace Moores_Law
         static void Main(string[] args)
         {
             List<TagsModel> tags = TagsModel.ProcessFile("c:\\TestFile.csv");
+            string outputFile = "C:\\OutputTestFile.csv";
 
             var tagEnums = Enum.GetValues(typeof(Tagging)).Cast<Tagging>().Select(x => x.ToString()).ToList();
             foreach (var item in tagEnums)
             {
                 var sortedTags = tags.Select(x => x.item).OrderBy(x => x).ToArray();
+                var csv = new StringBuilder();
                 foreach (var tag in sortedTags)
                 {
                     var sortedTag = TagsModel.SortedTag(tag);
+                    string Id = item;
+                    DateTime now = DateTime.Now;
+                    double tagstddev = TagsModel.GetStandardDeviation(sortedTag);
+                    double tagrms = TagsModel.GetRootMeanSquare(sortedTag);
+                    double tagrateofchange = TagsModel.GetRateOfChange(sortedTag);
 
-                    double tag1stddev = TagsModel.GetStandardDeviation(tagArray);
-                    double tag1rms = TagsModel.GetRootMeanSquare(tagArray);
-                    double tag1rateofchange = TagsModel.GetRateOfChange(tagArray);
+                    var newline = string.Format("{0}, {1}, {2}, {3}, {4}", Id, now, tagstddev, tagrms, tagrateofchange);
+                    csv.AppendLine(newline);
                 }
+
+                File.WriteAllText(outputFile, csv.ToString());
             }
 
 
